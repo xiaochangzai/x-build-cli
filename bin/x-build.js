0c6d4162ae02e37579b2674c83292eda03b11480
zwj <zhu.wenjian@vmicloud.com>
2018-06-23 19:54:31 +0800

    '修改了Github地址'

--- bin/x-build.js
+++ bin/x-build.js
@@ -4,13 +4,27 @@ const download = require('download-git-repo');
 const chalk = require('chalk');
 const ora = require('ora');
 
-program.version('0.1.0')
-    .option('-i, init [name]', '初始化x-build项目')
+program.version('0.1.1')
+    .option('i, init [name]', '初始化x-build项目')
     .parse(process.argv);
 
+    const nameQuestion = {
+        type: 'input',
+        message: `项目名称: `,
+        name: 'name',
+        default: 'x-build'
+      };
+      
+      const versionQuestion = {
+        type: 'input',
+        message: `初始版本: `,
+        name: 'version',
+        default: '0.0.1'
+      };
+
 if (program.init) { 
-    const spinner = ora('正在从github下载x-build').start(); 
-    download('https://github.com/xiaochangzai/TypeScript-Vue-Starter', program.init, function (err) { 
+    const spinner = ora('正在创建项目' + program.init).start(); 
+    download('xiaochangzai/MyDemo1', program.init, function (err) { 
         if (!err) { 
             // 可以输出一些项目成功的信息 
             console.info(chalk.blueBright('下载成功'));
